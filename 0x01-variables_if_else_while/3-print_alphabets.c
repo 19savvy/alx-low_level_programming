cd333a6fbd2cfb4c33173a9c55317f5155593fbf
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-17 15:03:35 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+*main - Entry point
+*
+*Return: 0
+*/
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		putchar('\n');
+		letter++;
+	}
+	return (0);
+}	
