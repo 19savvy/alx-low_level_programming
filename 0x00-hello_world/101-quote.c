f0c91a36f81748e82e4e20f24bbb372799ec5bf3
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-17 03:14:37 -0800

    print out a sentence

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\"\
- - Dora Korpar, 2015-10-19", 59);
+	write(stderr, "and that piece of art is useful\
+\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
