729a2d5cb1780b28c63d5271b7861953b56a514f
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-17 03:30:33 -0800

    print out a sentence

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	write(stderr, "and that piece of art is useful\
-\" - Dora Korpar, 2015-10-19", 59);
-	return (1);
+  int len;
+  char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+  len = 59;
+  if (write(STDERR_FILENO, msg, len) != len)
+    {
+      return (1);
+    }
+  return (1);
 }
+
