4f33fddc8067be4df8ad7468ca3db106261d1aa3
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-22 14:12:38 -0800

    check if alphabet is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase or not
+ *
+ * return: returns 1 if it is lowercase and 0 if its not*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
