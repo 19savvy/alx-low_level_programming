fa673688061f0e9614c1f788571a6fcecdb7fa39
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-22 12:50:34 -0800

    print alphabets in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,35 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-
-/**
- * print_message - print a message
- *
- * print_alphabet - prints alphabets
- */
-void print_message(void)
-{
-	printf("_putchar\n");
-}
-
-/**
- * print_alphabet - print aphabets
- *
- * Return: Always 0.
- */
-void print_alphabet(void)
-{
-	char letter = 'a';
-
-	while (letter <= 'z')
-	{
-
-		printf("%c", letter);
-		letter++;
-
-	}
-	printf("\n");
-
-}
-#endif /* MAIN_H*/
-
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
