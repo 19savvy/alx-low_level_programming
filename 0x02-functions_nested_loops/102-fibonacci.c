a76fc5d657178431c742bd0f8547ea347317b5c8
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-23 09:03:51 -0800

    prints the first 50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	while (i < 50)
 	{
 		if (i == 0)
-		        printf("%ld", j);
+			printf("%ld", j);
 
 		else if (i == 1)
 			printf(", %ld", k);
@@ -24,7 +24,6 @@ int main(void)
 			printf(", %ld", k);
 		}
 		i++;
-		
 	}
 	printf("\n");
 	return (0);
