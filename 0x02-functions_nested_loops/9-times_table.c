9e180bd5b44e4f7f75ba01b7939968107cf55bb7
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-23 00:59:11 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (a > 0)
+			if (c > 9)
 			{
 				d = c % 10;
 				e = (c - d) / 10;
@@ -25,7 +25,7 @@ void times_table(void)
 
 
 			}
-			else 
+	         	else
 			{
 				if (b != 0)
 				{
