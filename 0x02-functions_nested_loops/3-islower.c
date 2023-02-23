e191ff4b030256bb775b442da2e9ce050cd7372d
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-22 21:54:44 -0800

    checks if a character is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,8 @@
 /**
  * _islower - checks if a character is lowercase or not
  * Return: returns 1 if it is lowercase and 0 if its not
- * c - the charcter that will be checked
- * */
+ * c: the charcter that will be checked
+ */
 
 
 int _islower(int c)
