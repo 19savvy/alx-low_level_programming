f385fb4742aafbc1b636552807b9287a42d76335
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-23 01:15:43 -0800

    return sum of two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * add - adds two integers 
- * @n1: first number
- * @n2: second number
+ *add - adds two integers 
+ *@n1: first number
+ *@n2: second number
  *Return: returns integer
  */
 
- int add(int n1, int n2)
+int add(int n1, int n2)
 {
 	return (n1 + n2);
 }
