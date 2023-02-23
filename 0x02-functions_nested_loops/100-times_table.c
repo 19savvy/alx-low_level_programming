80149f49e613ef8344138d45a56fb4d96b2a4627
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-23 02:59:52 -0800

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+*print_times_table - prints times table of input,
+*starting with 0.
+*@n: value to be multiplied
+*/
+
+void print_times_table(int n)
+{
+	int num, mlt, prd;
+
+	if (n >= 0; && n <=15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mlt = 0; mlt <= n; mlt++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = num * mlt;
+				if (prod <= 99)
+				{
+					_putchar(' ');
+				}
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				}
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
