3e639e407f8d3472cd2d3c73c3203675380c5e16
Ken Nwogu <kenedynwogu027@gmail.com>
2023-02-28 01:52:08 -0800

    generate random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,25 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
+/* main - prints random passwords
+ * @charset: contain the character set from which the password will be picked
+ * @password_length: specifies the length of the password
+ * return: alawys 0
+ */
+
 int main(void)
 {
-    // Define the characters that can be used in the password
+    /* Define the characters that can be used in the password */
     char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    // Define the length of the password
+    /* Define the length of the password */
     int password_length = 10;
 
-    // Seed the random number generator
+    /* Seed the random number generator */
     srand(time(NULL));
 
-    // Generate the password
+    /* Generate the password */
     char password[password_length + 1];
     for (int i = 0; i < password_length; i++) {
         password[i] = charset[rand() % (sizeof(charset) - 1)];
     }
     password[password_length] = '\0';
 
-    // Output the password
+    /* Output the password */
     printf("Random password: %s\n", password);
 
     return 0;
