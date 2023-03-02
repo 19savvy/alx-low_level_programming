a8bb8eb2cf9ee887b253dc026cf9ca86c357fb1d
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-01 18:14:23 -0800

    concatenate srings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - a function to concatenate one string to another
+ * @dest: the destination
+ * @src: the source
+ * return: returns the resulting string
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	char *tmp = *dest;
+
+	while(*dest)
+	{
+		*dest++;
+	}
+
+	while(*src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = '\0';
+
+	return (temp);
+
+}
