9107cc3708340c856e6a085a0d859971b2e76d5b
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-03 17:31:03 -0800

    change start of every word to uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *s)
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
 
-		if (check_seperator(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
 		i++;
 	}
@@ -24,18 +24,18 @@ char *cap_string(char *s)
 
 /**
  * check_seperators - Separators of words: space, tabulation, new line,
- * ,, ;, ., !, ?, \", (, ), {, and }
+ * ,, ;, ., !, ?, ", (, ), {, and }
  * @c: an input character
  * Return: 1 if seperator, 0 otherwise
  */
-int check_seperator(char c)
+int check_seperators(char c)
 {
-	int k = 0;
+	int i = 0;
 	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
 		'"', '(', ')', '{', '}' };
-	for (; k < 13; k++)
+	for (; i < 13; i++)
 	{
-		if (c == seperators[k])
+		if (c == seperators[i])
 			return (1);
 	}
 	return (0);
