b5ebde7d2429d6ba4a54cc30223749d1c21291b7
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-03 17:31:52 -0800

    change start of every word to uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,7 +24,7 @@ char *cap_string(char *s)
 
 /**
  * check_seperators - Separators of words: space, tabulation, new line,
- * ,, ;, ., !, ?, ", (, ), {, and }
+ * ,, ;, ., !, ?, \", (, ), {, and }
  * @c: an input character
  * Return: 1 if seperator, 0 otherwise
  */
