3115c6d3789facd3a26d5fb24f4a548890fd67e3
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-03 17:25:15 -0800

    change start of every word to uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -38,7 +38,7 @@ char *cap_string(char *);
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
 
-		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+		if (check_seperator(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
 		i++;
 	}
