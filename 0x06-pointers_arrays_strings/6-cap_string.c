389952570bcfa4c6f6a31805dd15ce73398dabe3
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-03 17:38:19 -0800

    change start of every word to uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-int check_seperator(char c);
+int check_seperators(char c);
 
 /**
  * cap_string - a function that capitalizes all words of a string.
@@ -14,7 +14,7 @@ char *cap_string(char *s)
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
 
-		if (check_seperator(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
 		i++;
 	}
@@ -28,14 +28,14 @@ char *cap_string(char *s)
  * @c: an input character
  * Return: 1 if seperator, 0 otherwise
  */
-int check_seperator(char c)
+int check_seperators(char c)
 {
 	int i = 0;
-	char seperator[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
 		'"', '(', ')', '{', '}' };
 	for (; i < 13; i++)
 	{
-		if (c == seperator[i])
+		if (c == seperators[i])
 			return (1);
 	}
 	return (0);
