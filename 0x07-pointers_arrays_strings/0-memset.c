d618f29243d11f915046cbb77953e5b199e03f8c
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-06 07:59:16 -0800

    replace a location with a byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/* _memset - defines the function
+ * @s: pointer to the location to be filled
+ * @b: byte to fill the location with
+ * @n: number of bytes to be filled
+ * @start: points to the same location as s
+ * @return: beginnig of the location to be filled
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *start = s;
+
+	while(n--)
+	{
+		*s = b;
+		s++;
+	}
+	return start;
+}
