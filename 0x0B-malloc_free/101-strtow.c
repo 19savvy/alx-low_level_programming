4e01ab9ec4802aa7c90ab59ccf84979496de0cdc
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-14 05:23:33 -0700

    str tow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,19 +9,19 @@
  */
 static int count_words(char *str)
 {
-    int count = 0;
+	int count = 0;
 
-    while (*str)
-    {
-        while (*str == ' ')
-            str++;
-        if (*str != '\0')
-            count++;
-        while (*str && *str != ' ')
-            str++;
-    }
+	while (*str)
+	{
+		while (*str == ' ')
+			str++;
+		if (*str != '\0')
+			count++;
+		while (*str && *str != ' ')
+			str++;
+	}
 
-    return (count);
+	return (count);
 }
 
 /**
@@ -33,46 +33,45 @@ static int count_words(char *str)
  */
 char **strtow(char *str)
 {
-    char **array;
-    int i, j, k, len, count;
+	char **array;
+	int i, j, k, len, count;
 
-    if (str == NULL || *str == '\0')
-        return (NULL);
+	if (str == NULL || *str == '\0')
+		return (NULL);
 
-    count = count_words(str);
-    if (count == 0)
-        return (NULL);
+	count = count_words(str);
+	if (count == 0)
+		return (NULL);
 
-    array = malloc(sizeof(char *) * (count + 1));
-    if (array == NULL)
-        return (NULL);
+	array = malloc(sizeof(char *) * (count + 1));
+	if (array == NULL)
+		return (NULL);
 
-    for (i = 0, k = 0; str[i]; i++)
-    {
-        if (str[i] == ' ')
-            continue;
+	for (i = 0, k = 0; str[i]; i++)
+	{
+		if (str[i] == ' ')
+			continue;
 
-        for (j = i, len = 0; str[j] && str[j] != ' '; j++, len++)
-            ;
+		for (j = i, len = 0; str[j] && str[j] != ' '; j++, len++)
+			;
 
-        array[k] = malloc(sizeof(char) * (len + 1));
-        if (array[k] == NULL)
-        {
-            for (j = 0; j < k; j++)
-                free(array[j]);
-            free(array);
-            return (NULL);
-        }
+		array[k] = malloc(sizeof(char) * (len + 1));
+		if (array[k] == NULL)
+		{
+			for (j = 0; j < k; j++)
+				free(array[j]);
+			free(array);
+			return (NULL);
+		}
 
-        for (j = i, len = 0; str[j] && str[j] != ' '; j++, len++)
-            array[k][len] = str[j];
-        array[k][len] = '\0';
-        k++;
-        i = j;
-    }
+		for (j = i, len = 0; str[j] && str[j] != ' '; j++, len++)
+			array[k][len] = str[j];
+		array[k][len] = '\0';
+		k++;
+		i = j;
+	}
 
-    array[k] = NULL;
-    array = realloc(array, sizeof(char *) * (k + 1));
-    return (array);
+	array[k] = NULL;
+	return (array);
 }
 
