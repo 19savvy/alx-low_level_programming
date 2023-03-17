aa12d97efdb4397a3665a14650025c98b60cbbf7
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-16 19:50:40 -0700

    multiply two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 
 /**
@@ -9,6 +8,7 @@
  *
  * Return: 0 on success, 98 on failure
  */
+
 int main(int argc, char **argv)
 {
 	int num1, num2, answer;
@@ -21,18 +21,20 @@ int main(int argc, char **argv)
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	int i = isdigit(num1);
-	int k = isdigit(num2);
+	char i = '0';
 
-	if (i == 0 || k == 0)
+	for (; i <= '9'; i++)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	else
-	{
-		answer = num1 * num2;
-		printf("%d\n", answer);
+		if (num1 != i || num2 != i)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		else
+		{
+			answer = num1 * num2;
+			printf("%d\n", answer);
+		}
 	}
 
 	return (0);
