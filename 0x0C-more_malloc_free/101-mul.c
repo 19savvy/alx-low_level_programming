c75efd288c275a870b1454d51d3802fc1a28f30e
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-16 18:03:58 -0700

    multiply two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,10 +2,22 @@
 #include <stdlib.h>
 
 /**
- * main - Check the code
+ * _isdigit - checks if a character is a digit
+ * @c: character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: arguments
- * Return: Nothing
+ *
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char **argv)
 {
@@ -14,14 +26,26 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-	else
+
+	for (int i = 1; i < argc; i++)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
+		for (int j = 0; argv[i][j]; j++)
+		{
+			if (!_isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (98);
+			}
+		}
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+
+	return (0);
 }
+
