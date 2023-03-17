eb97b988ed4305aaa881daf1d9fe4dc79ea7887c
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-16 19:59:33 -0700

    multiply two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,41 +2,26 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two positive numbers
+ * main - Check the code
  * @argc: number of arguments
  * @argv: arguments
- *
- * Return: 0 on success, 98 on failure
+ * Return: Nothing
  */
-
 int main(int argc, char **argv)
 {
-	int num1, num2, answer;
+	int num1, num2, result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	char i = '0';
-
-	for (; i <= '9'; i++)
+	else
 	{
-		if (num1 != i || num2 != i)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-		else
-		{
-			answer = num1 * num2;
-			printf("%d\n", answer);
-		}
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
 	}
-
-	return (0);
 }
-
