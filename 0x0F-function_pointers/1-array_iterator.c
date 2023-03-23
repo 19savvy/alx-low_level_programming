85c103c16ab18cc97560ba2baf770be86066c923
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-22 21:36:57 -0700

    carry out a function on all elements of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,10 +10,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-
+	int i = 0;
 	if (action == NULL || array == NULL)
 		return;
 
-	for (unsigned int i = 0; i < size; i++)
+	for (; i < size; i++)
 		action(array[i]);
 }
