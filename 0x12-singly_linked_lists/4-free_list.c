bd540c2c44c808d27b0e09831a2d811193f72ea2
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-31 05:30:02 -0700

    task 5

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stlib.h>
+#include <strings.h>
+/**
+ * free_list - a function that frees a list
+ * @head: A pointer to the first node of the list to free.
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+	list_t *nextList;
+
+	while (temp)
+	{
+		nextList = temp->next;
+		free(temp->str);
+		free(temp);
+		temp = nextList;
+	}
+
+}
