83a140286751b23836b944dfef4d6b01b1d0681a
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-31 03:42:29 -0700

    fix 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,12 +3,9 @@
 #include <stdlib.h>
 
 /**
- * add_node - A function that adds a node to the head of linked list
  * stringLength - a function that return the number of strings
- * @head: pointer to a list_t pointer that points to the head struct
- * @c: pointer to a string
- * @str: pointer to string that adds as node
- * Return: pointer to new head of list, NULL on failure
+ * @c: pointer to the string that adds a node
+ * Return: return 1 on success
  */
 int stringLength(const char *c)
 {
@@ -21,13 +18,18 @@ int stringLength(const char *c)
 		i++;
 	}
 	return (i);
-	
 }
+/**
+ * add_node - A function that adds a node to the head of linked list
+ * @head: pointer to a list_t pointer that points to the head struct
+ * @str: pointer to the string that adds a node
+ * Return: pointer to new head of list, NULL on failure
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node;
-	node = malloc(sizeof(list_t));
 
+	node = malloc(sizeof(list_t));
 	if (node == NULL)
 		return (NULL);
 	node->str = strdup(str);
