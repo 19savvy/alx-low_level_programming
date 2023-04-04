d6e3e4bca243ffd4411bd591f21ae318e0505a65
Ken Nwogu <kenedynwogu027@gmail.com>
2023-04-04 15:59:09 -0700

    fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,40 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_listint_safe - A function that prints the elementsin a
- * linked list
+ * print_listint_safe - A function that prints the elements in a list
+ *                      and detects if there is a loop in the list.
  * @head: A pointer to listint_t structure
- * Return: The number of nodes.n or 98 on failure
+ * Return: The number of nodes. Exits with 98 on failure
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-	const listint_t *temp = head, *temp2 = head;
+    size_t nodes = 0;
+    const listint_t *current = head;
+    const listint_t *visited[1024] = {NULL};  // Hash table to keep track of visited nodes
 
-	if (head == NULL)
-		exit(98);
+    while (current != NULL)
+    {
+        // Check if the current node has already been visited
+        for (size_t i = 0; i < nodes; i++)
+        {
+            if (visited[i] == current)
+            {
+                printf("-> [%p] %d\n", (void *)current, current->n);
+                exit(98);
+            }
+        }
 
-	while (one && two && two->next && head)
-	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
-		}
+        // If the current node has not been visited, add it to the hash table
+        visited[nodes] = current;
 
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nodes++;
-	}
-	head = NULL;
-	return (nodes);
+        // Print the current node and move to the next node
+        printf("[%p] %d\n", (void *)current, current->n);
+        current = current->next;
+        nodes++;
+    }
+
+    return (nodes);
 }
+
