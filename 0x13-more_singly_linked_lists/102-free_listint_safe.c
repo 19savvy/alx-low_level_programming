0a6db22b6f6c6f7f65dc351bbfe6fa9e6cfd75ec
Ken Nwogu <kenedynwogu027@gmail.com>
2023-04-05 01:07:22 -0700

    fix

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,43 +3,36 @@
 #include <stdlib.h>
 
 /**
- * free_listint_safe - A function that frees a list
- * @h: A pointer listint_t structure
- * Return: The size of the list that was free'd
+ * free_listint_safe - Frees a linked list.
+ *
+ * @h: A pointer to a pointer to the first node of the list.
+ *
+ * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listint_t *temp;
+    size_t nodes = 0;
+    listint_t *temp;
 
-	temp = *h;
-	while (temp)
-	{
-		temp = *h;
-		temp = temp->next;
-		free_list(temp);
-		count++;
-	}
-	*h = NULL;
+    while (*h != NULL)
+    {
+        nodes++;
 
-	return (count);
+        if ((*h)->next >= *h)
+        {
+            printf("Freeing the list: %d\n", (*h)->n);
+            free(*h);
+            *h = NULL;
+            break;
+        }
+
+        temp = (*h)->next;
+        free(*h);
+        *h = temp;
+    }
+
+    *h = NULL;
+
+    return (nodes);
 }
 
-/**
- * free_list - A function that frees a listint_t recursively
- * @head: A pointer to the listint_t structure
- * Return: Nothing
- */
-void free_list(listint_t *head)
-{
-	listint_t *temp;
-
-	if (head)
-	{
-		temp = head;
-		temp = temp->next;
-		free(temp);
-		free_list(temp);
-	}
-	free(head);
-}
