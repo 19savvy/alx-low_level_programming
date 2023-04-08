a820384f65c5a113938e35f3b6e5f884d311ef07
Ken Nwogu <kenedynwogu027@gmail.com>
2023-04-08 04:35:20 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,9 +14,11 @@ unsigned int binary_to_uint(const char *b)
 
 	conv = strtoul(b, &fail, 2);
 
+	if (b == NULL)
+		return (0);
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '1' && b[i] != '0' || b == NULL)
+		if (b[i] !='1' || b[i] != '0')
 		{
 			return (0);
 		}
