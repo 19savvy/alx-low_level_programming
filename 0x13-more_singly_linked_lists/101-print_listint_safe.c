8cd21bc030a4a5e38ecf914d8edf022a44368e37
nwogu kennedy <kennedynwogu027@gmail.com>
2023-08-31 01:16:19 +0100

    fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,48 +1,56 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list of integers and detects if there
- * is a loop in the list
- * @head: pointer to the head of the linked list
- * Return: the number of nodes in the list
+ *print_listint_safe - prints a listint_t linked list.
+ *@head: Pointer to the first node
+ *Return: Number of nodes in the linked list
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *current, *temp;
+	const listint_t *list[10000];
+	size_t nodes = 0;
 
-	if (head == NULL)
-		exit(98);
-
-	current = head;
-	while (current != NULL)
+	while (head)
 	{
-		count++;
-		printf("[%p] %d\n", (void *)current, current->n);
-
-		temp = current;
-		current = current->next;
-
-		if (temp < current)
-			continue;
-
-		if (temp > current)
+		if (nodes >= sizeof(list) / sizeof(list[0]))
 		{
-			current = head;
-			while (temp != NULL)
-			{
-				printf("[%p] %d\n", (void *)temp, temp->n);
-				count++;
-				temp = temp->next;
-
-				if (temp == head)
-					return (count);
-			}
+			fprintf(stderr, "List is too large.\n");
 			exit(98);
 		}
+
+		if (contains_node(list, nodes, head))
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			return (nodes);
+		}
+
+		list[nodes++] = head;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-	return (count);
+
+	return (nodes);
+}
+
+/**
+ * contains_node - checks if a node exists in an array of nodes
+ * @list: Array of pointers to listint_t nodes
+ * @size: Size of the array
+ * @node: Pointer to the node to check
+ * Return: 1 if the node exists in the array, 0 otherwise
+ */
+int contains_node(const listint_t **list, size_t size, const listint_t *node)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (list[i] == node)
+			return (1);
+	}
+	return (0);
 }
 
